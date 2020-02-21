1294ba8130fdd4f11ae9ef197e6f10a76ac776f8
dnzvsh <zuevdanil@gmail.com>
2020-02-21 19:56:38 +0700

    Create function initial_state

--- chessviz.c
+++ chessviz.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-typedef struct
-{
+typedef struct {
     char form;
 } Figure;
 
-int main()
+void initial_state()
 {
     Figure king[2] = {'K', 'k'};
     Figure queen[2] = {'Q', 'q'};
@@ -18,69 +17,45 @@ int main()
     Figure arr[8][8];
     printf("   1 2 3 4 5 6 7 8\n");
     printf("  ________________\n");
-    for (int i = 0; i < 8; i++)
-    {
+    for (int i = 0; i < 8; i++) {
         printf("%c| ", i + 65);
- 
-        for (int j = 0; j < 8; j++)
-        {
-            arr[i][j].id = i + 1;
-            arr[i][j].letter = j + 1;
-            if (((i == 0) && (j == 0)) || ((i == 0) && (j == 7)))
-            {
+        for (int j = 0; j < 8; j++) {
+            if (((i == 0) && (j == 0)) || ((i == 0) && (j == 7))) {
                 arr[i][j] = *rook;
-            }
-            else if (((i == 7) && (j == 7)) || ((i == 7) && (j == 0)))
-            {
+            } else if (((i == 7) && (j == 7)) || ((i == 7) && (j == 0))) {
                 arr[i][j] = rook[1];
-            }
-            else if (((i == 0) && (j == 1)) || ((i == 0) && (j == 6)))
-            {
+            } else if (((i == 0) && (j == 1)) || ((i == 0) && (j == 6))) {
                 arr[i][j] = *knight;
-            }
-            else if (((i == 7) && (j == 6)) || ((i == 7) && (j == 1)))
-            {
+            } else if (((i == 7) && (j == 6)) || ((i == 7) && (j == 1))) {
                 arr[i][j] = knight[1];
-            }
-            else if (((i == 0) && (j == 2)) || ((i == 0) && (j == 5)))
-            {
+            } else if (((i == 0) && (j == 2)) || ((i == 0) && (j == 5))) {
                 arr[i][j] = *bishop;
-            }
-            else if (((i == 7) && (j == 5)) || ((i == 7) && (j == 2)))
-            {
+            } else if (((i == 7) && (j == 5)) || ((i == 7) && (j == 2))) {
                 arr[i][j] = bishop[1];
-            }
-            else if ((i == 0) && (j == 3))
-            {
+            } else if ((i == 0) && (j == 3)) {
                 arr[i][j] = *king;
-            }
-            else if ((i == 7) && (j == 4))
-            {
+            } else if ((i == 7) && (j == 4)) {
                 arr[i][j] = king[1];
-            }
-            else if ((i == 0) && (j == 4))
-            {
+            } else if ((i == 0) && (j == 4)) {
                 arr[i][j] = *queen;
-            }
-            else if ((i == 7) && (j == 3))
-            {
+            } else if ((i == 7) && (j == 3)) {
                 arr[i][j] = queen[1];
-            }
-            else if (i == 1)
-            {
+            } else if (i == 1) {
                 arr[i][j] = *pawn;
-            }
-            else if (i == 6)
-            {
+            } else if (i == 6) {
                 arr[i][j] = pawn[1];
-            }
-            else
-            {
+            } else {
                 arr[i][j] = nan;
             }
- 
+
             printf("%c ", arr[i][j]);
         }
         printf("\n");
     }
+}
+
+int main()
+{
+    initial_state();
+    return 0;
 }
