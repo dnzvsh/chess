49adc65e3b31d1159b0402d9e26f5840ee3bbf03
dnzvsh <zuevdanil@gmail.com>
2020-03-13 18:08:50 +0700

    Remake function initial_state && delete struct

--- chessviz.c
+++ chessviz.c
@@ -1,54 +1,38 @@
-#include <stdbool.h>
 #include <stdio.h>
 
-typedef struct {
-    char form;
-} Figure;
-
-void initial_state()
+void initial_board(char chess[][8])
 {
-    Figure king[2] = {{'K'}, {'k'}};
-    Figure queen[2] = {{'Q'}, {'q'}};
-    Figure bishop[2] = {{'B'}, {'b'}};
-    Figure knight[2] = {{'N'}, {'n'}};
-    Figure rook[2] = {{'R'}, {'r'}};
-    Figure pawn[2] = {{'P'}, {'p'}};
-    Figure nan = {'0'};
-    Figure arr[8][8];
-    printf("   1 2 3 4 5 6 7 8\n");
-    printf("  ________________\n");
+    chess[0][0] = 'R';
+    chess[0][1] = 'N';
+    chess[0][2] = 'B';
+    chess[0][3] = 'K';
+    chess[0][4] = 'Q';
+    chess[0][5] = 'B';
+    chess[0][6] = 'N';
+    chess[0][7] = 'R';
     for (int i = 0; i < 8; i++) {
-        printf("%c| ", i + 65);
+        chess[1][i] = 'P';
+        chess[6][i] = 'p';
+    }
+    chess[7][0] = 'r';
+    chess[7][1] = 'n';
+    chess[7][2] = 'b';
+    chess[7][3] = 'k';
+    chess[7][4] = 'q';
+    chess[7][5] = 'b';
+    chess[7][6] = 'n';
+    chess[7][7] = 'r';
+    for (int i = 2; i < 6; i++) {
         for (int j = 0; j < 8; j++) {
-            if (((i == 0) && (j == 0)) || ((i == 0) && (j == 7))) {
-                arr[i][j] = *rook;
-            } else if (((i == 7) && (j == 7)) || ((i == 7) && (j == 0))) {
-                arr[i][j] = rook[1];
-            } else if (((i == 0) && (j == 1)) || ((i == 0) && (j == 6))) {
-                arr[i][j] = *knight;
-            } else if (((i == 7) && (j == 6)) || ((i == 7) && (j == 1))) {
-                arr[i][j] = knight[1];
-            } else if (((i == 0) && (j == 2)) || ((i == 0) && (j == 5))) {
-                arr[i][j] = *bishop;
-            } else if (((i == 7) && (j == 5)) || ((i == 7) && (j == 2))) {
-                arr[i][j] = bishop[1];
-            } else if ((i == 0) && (j == 3)) {
-                arr[i][j] = *king;
-            } else if ((i == 7) && (j == 4)) {
-                arr[i][j] = king[1];
-            } else if ((i == 0) && (j == 4)) {
-                arr[i][j] = *queen;
-            } else if ((i == 7) && (j == 3)) {
-                arr[i][j] = queen[1];
-            } else if (i == 1) {
-                arr[i][j] = *pawn;
-            } else if (i == 6) {
-                arr[i][j] = pawn[1];
-            } else {
-                arr[i][j] = nan;
-            }
+            chess[i][j] = ' ';
+        }
+    }
 
-            printf("%c ", arr[i][j].form);
+    printf("   A B C D E F G H\n   _______________\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%d |", i + 1);
+        for (int j = 0; j < 8; j++) {
+            printf("%c ", chess[i][j]);
         }
         printf("\n");
     }
@@ -56,6 +40,7 @@ void initial_state()
 
 int main()
 {
-    initial_state();
+    char chess[8][8] = {{' '}};
+    initial_board(chess);
     return 0;
 }
